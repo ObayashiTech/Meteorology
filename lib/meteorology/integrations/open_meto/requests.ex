b31044f1e3627cddc1e446792c240bf088af495a
OBAYASHI TECH <obayashi.tech@gmail.com>
2025-04-25 23:54:27 -0300

    choose client by env

--- lib/meteorology/integrations/open_meto/requests.ex
+++ lib/meteorology/integrations/open_meto/requests.ex
@@ -3,12 +3,22 @@ defmodule Meteorology.Integrations.OpenMeteo.HTTP.Requests do
   Provides high-level HTTP request functions for interacting with the Open-Meteo API.
   """
 
-  alias Meteorology.Integrations.OpenMeteo.HTTP.{Client, Helpers}
+  alias Meteorology.Integrations.OpenMeteo.HTTP.Helpers
 
-  def get_max_temperature(latitude, longitude, timezone, client \\ Client) do
+  def get_max_temperature(latitude, longitude, timezone) do
     latitude
     |> Helpers.build_url(longitude, timezone)
-    |> client.get()
+    |> client().get()
     |> Helpers.parse_response()
+  end
+
+  defp client do
+    case Mix.env() do
+      :test ->
+        Meteorology.Integrations.OpenMeteo.HTTP.MockClient
+
+      _ ->
+        Meteorology.Integrations.OpenMeteo.HTTP.Client
+    end
   end
 end
